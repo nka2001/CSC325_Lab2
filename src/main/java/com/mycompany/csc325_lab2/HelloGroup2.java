c6a51c97ef7c16c75e2246bb7636866823e9aeb6
MaazHussaini99 <maazhussaini@outlook.com>
2022-02-08 13:54:32 -0500

    Initial Commit

---
+++ src/main/java/com/mycompany/csc325_lab2/HelloGroup2.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.csc325_lab2;
+
+/**
+ *
+ * @author maazh
+ */
+public class HelloGroup2 {
+    public static void main(String[] args) {
+        System.out.println("Hello Group 2");
+    }
+}
